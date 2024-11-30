a1b4e2f5c13dd357fe03744b26f43298d0a5ef21
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-11-30 07:02:41 +0530

    Conditional Rendering with Arrays (map)

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const isLoggedIn = true;
-  const isTernoryOp = true;
-
-  let content;
-  if (isLoggedIn) {
-    content = <h1>Welcome Back!</h1>;
-  } else {
-    content = <h1>Please Log In</h1>;
-  }
-
-  return <div>{content}
-    <div>
-      {isTernoryOp ? <h1>Ternory Operator!</h1> : <h1>Not a ternory operator</h1>}
-    </div>
-  </div>;
+  const items = ["Apple", "Banana", "Cherry", "Date"];
+  const filter = "A";
+  return (
+    <>
+      <h2>Array Rendering</h2>
+      <ul>
+        {items.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <h2> Conditional Rendering with Arrays</h2>
+      <ul>
+        {items
+          .filter(item => item.startsWith(filter))
+          .map(item => (
+            <li key={item}>{item}</li>
+          ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
