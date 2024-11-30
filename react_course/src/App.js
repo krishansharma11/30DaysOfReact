3f2b52748aa8f7d69bff55829e6a0c041ebcb25a
krishan kumar sharma <krishan.sharma02@nagarro.com>
2024-11-30 07:24:00 +0530

    rendering with multiple data

--- react_course/src/App.js
+++ react_course/src/App.js
@@ -4,6 +4,17 @@ import React from 'react';
 function App() {
   const items = ["Apple", "Banana", "Cherry", "Date"];
   const filter = "A";
+  const fruits = [
+    { id: 1, name: "Apple" },
+    { id: 2, name: "Banana" },
+    { id: 3, name: "Cherry" },
+    { id: 4, name: "Date" },
+  ];
+
+
+  function Fruit({ name }) {
+    return <li>{name}</li>;
+  }
   return (
     <>
       <h2>Array Rendering</h2>
@@ -22,6 +33,18 @@ function App() {
           .map(item => (
             <li key={item}>{item}</li>
           ))}
+      </ul>
+      <h2>Array with multiple data</h2>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>{fruit.name}</li>
+        ))}
+      </ul>
+      <h2>Rendering Components in a List</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <Fruit key={index} name={fruit.name} />
+        ))}
       </ul>
     </>
   );
